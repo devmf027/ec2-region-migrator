84990f7ab5e9e0c6309bc6c776b03d72fc0fd103
dariel.mizrachi <dariel027@gmail.com>
2023-11-10 19:52:31 +0200

    Adds fucntions to get vpc, subnet amd security group data

--- app/get_data_functions.py
+++ app/get_data_functions.py
@@ -17,4 +17,38 @@ def get_ec2_instance_data(instance_id):
     return response
 
 
+def get_vpc_data(vpc_id):
+    """
+    Retrieve information about a VPC by its ID using Boto3.
+
+    :param vpc_id: The ID of the VPC to retrieve information for.
+    :returns: A dictionary containing information about the specified VPC.
+    """
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_vpcs(VpcIds=[vpc_id])
+    return response
+
+
+def get_security_group_data(group_id):
+    """
+    Retrieve information about a security group by its ID using Boto3.
+
+    :param group_id: The ID of the security group to retrieve information for.
+    :returns: A dictionary containing information about the specified security group.
+    """
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_security_groups(GroupIds=[group_id])
+    return response
+
+
+def get_subnet_data(subnet_id):
+    """
+    Retrieve information about a subnet by its ID using Boto3.
+
+    :param subnet_id: The ID of the subnet to retrieve information for.
+    :returns: A dictionary containing information about the specified subnet.
+    """
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_subnets(SubnetIds=[subnet_id])
+    return response
 
