d8417dcd6d44c1ebab8b8cb80e14b3477ef9e459
dariel.mizrachi <dariel027@gmail.com>
2023-11-11 15:10:43 +0200

    Changes the ec2_info dict keys to boto3 naming format

--- app/get_data_functions.py
+++ app/get_data_functions.py
@@ -12,7 +12,7 @@ def get_ec2_instance_data(instance_id):
     :returns: A dictionary containing information about the specified EC2 instance.
     """
 
-    ec2 = boto3.client('ec2')
+    ec2 = boto3.client("ec2")
     response = ec2.describe_instances(InstanceIds=[instance_id])
     return response
 
@@ -24,7 +24,7 @@ def get_vpc_data(vpc_id):
     :param vpc_id: The ID of the VPC to retrieve information for.
     :returns: A dictionary containing information about the specified VPC.
     """
-    ec2 = boto3.client('ec2')
+    ec2 = boto3.client("ec2")
     response = ec2.describe_vpcs(VpcIds=[vpc_id])
     return response
 
@@ -36,7 +36,7 @@ def get_security_group_data(group_id):
     :param group_id: The ID of the security group to retrieve information for.
     :returns: A dictionary containing information about the specified security group.
     """
-    ec2 = boto3.client('ec2')
+    ec2 = boto3.client("ec2")
     response = ec2.describe_security_groups(GroupIds=[group_id])
     return response
 
@@ -48,65 +48,43 @@ def get_subnet_data(subnet_id):
     :param subnet_id: The ID of the subnet to retrieve information for.
     :returns: A dictionary containing information about the specified subnet.
     """
-    ec2 = boto3.client('ec2')
+    ec2 = boto3.client("ec2")
     response = ec2.describe_subnets(SubnetIds=[subnet_id])
     return response
 
 
-def extract_ec2_info(ec2_data):
+def extract_instance_info(ec2_data):
     """
     Extract and structure EC2 information from the AWS API response.
 
     This function takes the output of the `get_ec2_instance_data` function and extracts specific information
     to create a dictionary with the EC2 instance ID as the key and relevant data as values.
 
-    :param ec2_data: The AWS API response for an EC2 instance.
+    :param ec2_data: The AWS API response from describe EC2 instance.
     :returns: A dictionary with EC2 instance ID as the key and relevant data as values.
     """
     ec2_info = {}
 
-    if 'Reservations' in ec2_data:
-        for reservation in ec2_data['Reservations']:
-            for instance in reservation['Instances']:
-                instance_id = instance['InstanceId']
+    if "Reservations" in ec2_data:
+        for reservation in ec2_data["Reservations"]:
+            for instance in reservation["Instances"]:
+                instance_id = instance["InstanceId"]
 
                 # Extract the security group IDs from the SecurityGroups list
-                security_group_ids = [sg['GroupId']
-                                      for sg in instance.get('SecurityGroups', [])]
+                security_group_ids = [sg["GroupId"]
+                                      for sg in instance.get("SecurityGroups", [])]
 
                 ec2_info[instance_id] = {
-                    'ec2_name': instance.get('KeyName', ''),
-                    'ec2_type': instance['InstanceType'],
-                    'vpc_id': instance['VpcId'],
-                    'subnet_cidr': instance.get('SubnetId', ''),
-                    'security_group_id': security_group_ids,
-                    'tags_of_ec2': instance.get('Tags', [])
+                    "InstanceType": instance["InstanceType"],
+                    "VpcId": instance["VpcId"],
+                    "SubnetId": instance.get("SubnetId", ""),
+                    "PrivateIpAddress": instance.get("PrivateIpAddress", ""),
+                    "SecurityGroups": security_group_ids,
+                    "Tags": instance.get("Tags", []),
+                    
                 }
 
     return ec2_info
 
 
-def save_to_audit_file(resource_id, resource_type, data):
-    """
-    Save data to a JSON file in the audit directory with a timestamp.
 
-    This function takes resource ID, resource type, and data as input and saves the data to a JSON file in the audit directory.
-    The file name is constructed using the resource ID, type, and a timestamp.
-
-    :param resource_id: The ID of the AWS resource.
-    :param resource_type: The type of the AWS resource (e.g., "ec2", "vpc", "security_group").
-    :param data: The data to be saved to the JSON file.
-    :returns: None
-    """
-    # Get the current timestamp
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-
-    # Construct the file path with resource ID, type, and timestamp
-    file_path = f"audit/{resource_type}_{resource_id}_{timestamp}.json"
-
-    # Create the directory if it doesn't exist
-    os.makedirs(os.path.dirname(file_path), exist_ok=True)
-
-    # Save the data to the JSON file
-    with open(file_path, 'w') as file:
-        json.dump(data, file, indent=4, default=str)
