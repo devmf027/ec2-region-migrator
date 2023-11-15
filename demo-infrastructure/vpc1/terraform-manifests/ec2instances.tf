25a4618fcc40dd5cfd3958e2e4674f476d1fc1a6
dariel.mizrachi <dariel027@gmail.com>
2023-11-15 12:20:52 +0200

    Removes one ec2 instance module

--- demo-infrastructure/vpc1/terraform-manifests/ec2instances.tf
+++ demo-infrastructure/vpc1/terraform-manifests/ec2instances.tf
@@ -14,16 +14,3 @@ module "ec2_public_bastion" {
   tags = local.common_tags
 }
 
-module "ec2_public_web_server" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "5.5.0"
-
-  name          = "${local.name}-web-server"
-  ami           = data.aws_ami.amzlinux2.id
-  instance_type = "t2.micro"
-
-  subnet_id              = module.vpc.public_subnets[1]
-  vpc_security_group_ids = [module.public_web_server_sg.security_group_id]
-
-  tags = local.common_tags
-}
